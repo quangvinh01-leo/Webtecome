53f5ea925b209686e1a5d1a9a2e3a3b981e751c4
VinhLQ9 <vinhreal1996@gmail.com>
2021-07-25 15:20:55 +0700

    add modules Store

--- webtecome/urls.py
+++ webtecome/urls.py
@@ -16,17 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-# from .views import views
 from webtecome import views
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', views.home, name='home'),
-    # path('', include(views.home)),
+    path('store/', include('store.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
