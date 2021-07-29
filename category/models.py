89dd75940a19a377e3f7b5036cdd6a01a35c2ad4
VinhLQ9 <vinhreal1996@gmail.com>
2021-07-29 10:31:34 +0700

    work with product, context processor & product detail

--- category/models.py
+++ category/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Category(models.Model):
@@ -8,8 +10,11 @@ class Category(models.Model):
     cat_image = models.ImageField(upload_to='photos/categories',blank=True)
 
     class Meta:
-        verbose_name ='category'
+        verbose_name        ='category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('product_by_category', args =[self.slug])
 
     def __str__(self):
         return self.category_name
